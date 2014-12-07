f0cc4b9fd011af22052c28802402d045aff1eb8b
Dalikas <dalius.bagdonas@gmail.com>
2014-12-07 16:48:31 +0200

    nuėmiau išsipisinėjimus

--- wsgi/openshift/views.py
+++ wsgi/openshift/views.py
@@ -2,7 +2,7 @@ import os
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('home/index.html')
+    return render_to_response('home/home.html')
 
 def registruotis(request):
     return render_to_response('home/registruotis.html')
