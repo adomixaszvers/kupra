75b7456bc318d61770ff91fb3ee670dc1f817a4e
Adomas Jatužis <adomixaszvers@gmail.com>
2014-12-08 09:18:50 +0200

    Trying to fix static files serving

--- wsgi/openshift/urls.py
+++ wsgi/openshift/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-from django.conf.urls.static import static
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
@@ -17,5 +16,10 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
+if settings.DEBUG:
+    urlpatterns += patterns('', (r'^media\/(?P<path>.*)$',
+                                 'django.views.static.serve',
+                                 {'document_root': settings.MEDIA_ROOT}),
+                           )
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
