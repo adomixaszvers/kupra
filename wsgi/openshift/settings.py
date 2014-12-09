60dd14d910066316b4ab06f88ba6c57b664d5ff0
Adomas Jatužis <adomixaszvers@gmail.com>
2014-12-09 12:15:16 +0200

    Static files fixed

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -82,7 +82,10 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
+if ON_OPENSHIFT:
+    STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
+else:
+    STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -98,6 +101,10 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+)
+if not ON_OPENSHIFT:
+    STATICFILES_DIRS = (
+            os.path.join(PROJECT_DIR, '..', 'static'),
 )
 
 # List of finder classes that know how to find static files in
