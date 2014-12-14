ffac1359d10852ef6df21f73ea7e503e1ccebc10
Adomas Jatužis <adomixaszvers@gmail.com>
2014-12-14 19:45:16 +0200

    Much work no rest

--- wsgi/openshift/kupra/models.py
+++ wsgi/openshift/kupra/models.py
@@ -2,6 +2,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from uuid_upload_path import upload_to
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db import transaction
+
 
 
 # Create your models here.
@@ -58,12 +62,37 @@ class RecipeProduct(Product):
     recipe = models.ForeignKey('Recipe')
 
 
+@receiver(post_save, sender=RecipeProduct)
+@transaction.atomic
+def recipe_product_handle(sender, **kwargs):
+    if not kwargs.get('created'):
+        return
+    product = kwargs.get('instance')
+    name = product.name
+    recipe = product.recipe
+    unit = product.unit
+    q = RecipeProduct.objects.filter(name=name, recipe=recipe, unit=unit)
+    if q.count() <= 1:
+        return
+    quantity = q.aggregate(models.Sum('quantity')).values()[0]
+    if q.count() == 0:
+        return
+    product = q[0]
+    q = q.exclude(pk=product.pk)
+    q.delete()
+    product.quantity = quantity
+    product.save()
+    return
+
+
 class RecipeComment(models.Model):
     CHOICES = ((x, x) for x in range(1, 11))
     recipe = models.ForeignKey('kupra.Recipe')
     user = models.ForeignKey(User)
     score = models.IntegerField(choices=CHOICES)
     comment = models.TextField()
+    class Meta:
+        unique_together = ('user', 'recipe')
 
 
 class Recipe(models.Model):
